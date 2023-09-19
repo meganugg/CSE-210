e17fc7d6886f269b59037a488f0428c74ea7257a
meganugg <tbstrupp@gmail.com>
2023-09-19 13:38:26 -0600

    Completed Prep3, Prep4 and Prep5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guess = -50;
+        int guess_count = 0;
+        Console.WriteLine("Guess the number (1 - 100)");
+        while(guess != magic_number){
+            guess_count++;
+            String guess_string = Console.ReadLine();
+            guess = int.Parse(guess_string);
+            if(guess > magic_number){
+                Console.WriteLine("Too high");
+            }
+            if(guess < magic_number){
+                Console.WriteLine("Too low");
+            }
+        }
+        Console.WriteLine($"You guessed the number! ({magic_number})");
+        Console.WriteLine($"It took you {guess_count} guesses");
     }
 }
