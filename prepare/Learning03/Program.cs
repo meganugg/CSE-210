5453a619afbbdf1e5263ecfe444b5ad724f02f4a
meganugg <tbstrupp@gmail.com>
2023-10-11 11:45:07 -0600

    Forgot to test cases where no parameters or partial parameters were passed to the class. Modified program to satisfy those conditions. Actually finished the project this time

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Xml.Schema;
 
 class Program
@@ -8,22 +9,48 @@ class Program
         Console.WriteLine("Hello Learning03 World!");
         int select = 0;
         while(select != 2){
+            bool err1 = false;
+            bool err2 = false;
+            int n = 0;
+            int d = 0;
             Console.WriteLine("1. Fraction / Decimal");
             Console.WriteLine("2. Quit");
             Console.Write("> ");
             String selectString = Console.ReadLine();
-            select = int.Parse(selectString);
+            try{
+                select = int.Parse(selectString);
+            }catch{
+                Console.WriteLine("Select an option");
+            }
             switch(select){
                 case (1):
                     Console.Write("Enter a numerator: ");
                     String ns = Console.ReadLine();
-                    int n = int.Parse(ns);
+                    try{
+                        n = int.Parse(ns);
+                    }catch{
+                        err1 = true;
+                    }
                     Console.Write("Enter a denominator: ");
                     String ds = Console.ReadLine();
-                    int d = int.Parse(ds);
-                    Fraction f = new Fraction(n, d);
-                    Console.WriteLine(f.getFractionString());
-                    Console.WriteLine(f.getFractionDecimal());
+                    try{
+                        d = int.Parse(ds);
+                    }catch{
+                        err2 = true;
+                    }
+                    if(err1 == false && err2 == false){
+                        Fraction f = new Fraction(n, d);
+                        Console.WriteLine(f.getFractionString());
+                        Console.WriteLine(f.getFractionDecimal());
+                    }else if(err1 == true && err2 == true){
+                        Fraction f = new Fraction();
+                        Console.WriteLine(f.getFractionString());
+                        Console.WriteLine(f.getFractionDecimal());
+                    }else{
+                        Fraction f = new Fraction(1);
+                        Console.WriteLine(f.getFractionString());
+                        Console.WriteLine(f.getFractionDecimal());
+                    }
                     break;
                 case (2):
                     break;
