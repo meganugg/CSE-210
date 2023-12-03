218a2b155aec4b35edcdd174d1df2744571d3f7a
meganugg <tbstrupp@gmail.com>
2023-12-02 23:15:03 -0700

    Setup classes for youtube program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string stop = "";
+        List<int> nums = new List<int>();
+        nums.Add(2);
+        int integer = 0;
+        // Console.WriteLine(prime(nums, 7));
+        while(stop != "quit"){
+            integer++;
+            if(prime(nums, integer) == true){
+                Console.WriteLine(integer);
+                nums.Add(integer);
+            }
+            stop = Console.ReadLine();
+        }
+
+        bool prime(List<int> n, int num){
+            foreach(int nu in n){
+                if(num%nu == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
